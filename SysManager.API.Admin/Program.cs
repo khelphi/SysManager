7a1e1331901f8f5a1df23acb9a39cdb70bd74265
Denis Rodrigues Fernandes <khelphi@yahoo.com.br>
2022-06-17 18:59:26 -0300

    projeto Turma A

---
+++ SysManager.API.Admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysManager.API.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
